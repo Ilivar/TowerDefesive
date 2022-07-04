7ef04d0c6b8ee8b436e072f9534a557063a608b6
Giuliano Gioia <giulianogioia86@gmail.com>
2022-07-04 13:48:14 +0200

    ADD MinionMovement

---
+++ Assets/Scipts/MinionMovement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionMovement : MonoBehaviour
+{
+    public Vector3 targetPosition;
+    public float moveSpeed;
+    
+    
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        
+    }
+}
